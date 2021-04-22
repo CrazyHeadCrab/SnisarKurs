570a73d0682832ebc7e9185368f7226fa9edaaef
Snisar Maximilian OLD <maks1milian@mail.ru>
2021-04-22 17:55:10 +0300

    first commit

---
+++ windows/pages/enterance.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Configuration;
+using System.Data.SqlClient;
+
+
+
+namespace Barber.windows.pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Page1.xaml
+    /// </summary>
+    public partial class enterance : Page
+    {
+        public enterance()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void InBut_Click(object sender, RoutedEventArgs e)
+        {
+            string conect = ConfigurationManager.ConnectionStrings["defcon"].ConnectionString;
+            using(SqlConnection connection = new SqlConnection(conect))
+            {
+                connection.Open();
+                string command = "exec dbo.login_cheaks @log, @pas";
+                SqlCommand sqlCommand = new SqlCommand(command, connection);
+                sqlCommand.Parameters.AddWithValue("@log", loginBox.Text);
+                sqlCommand.Parameters.AddWithValue("@pas", PasBox.Password);
+                SqlDataReader sqlData = sqlCommand.ExecuteReader();
+                if (sqlData.Read())
+                {
+                    MessageBox.Show("вход");
+                }
+
+
+            }
+        }
+    }
+}
